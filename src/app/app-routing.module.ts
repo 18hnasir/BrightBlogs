85d310d57fcf79c2d8e4d7a260c6e90959d128aa
18hnasir <56739281+18hnasir@users.noreply.github.com>
2021-11-07 12:13:53 -0500

    Created home, movies, games, and TV page with static cards and text

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MoviesPageComponent } from './movies-page/movies-page.component';
+import { GamesPageComponent } from './games-page/games-page.component';
+import { TvPageComponent } from './tv-page/tv-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'movies-page', component: MoviesPageComponent}, 
+  { path: 'games-page', component: GamesPageComponent},
+  { path: 'tv-page', component: TvPageComponent}, 
+  { path: 'home-page', component: HomePageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { MoviesPageComponent } from './movies-page/movies-page.component';
+import { GamesPageComponent } from './games-page/games-page.component';
+import { TvPageComponent } from './tv-page/tv-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    MoviesPageComponent,
+    GamesPageComponent,
+    TvPageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
